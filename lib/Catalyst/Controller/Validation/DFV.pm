2a896d2560ab816ced4ac1b28bfee44a289403ce
Chisel <chisel@chizography.net>
2009-03-16 09:22:38 +0000

    Import of CHISEL/Catalyst-Controller-Validation-DFV-0.0.5 from CPAN.
    gitpan-cpan-distribution: Catalyst-Controller-Validation-DFV
    gitpan-cpan-version:      0.0.5
    gitpan-cpan-path:         CHISEL/Catalyst-Controller-Validation-DFV-0.0.5.tar.gz
    gitpan-cpan-author:       CHISEL
    gitpan-cpan-maturity:     released

--- lib/Catalyst/Controller/Validation/DFV.pm
+++ lib/Catalyst/Controller/Validation/DFV.pm
@@ -3,7 +3,7 @@ package Catalyst::Controller::Validation::DFV;
 use strict;
 use warnings;
 
-use version; our $VERSION = qv(0.0.4)->numify;
+use version; our $VERSION = qv(0.0.5)->numify;
 
 use base 'Catalyst::Controller';
 
